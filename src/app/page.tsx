050c865a8b8920aee7506c7850c6d8453ca93909
Gustavo-Bertti <bertinhobertti@hotmail.com>
2024-08-09 09:17:48 -0300

    terminando a parte do entrar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
@@ -7,27 +9,37 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-between p-24">
       <nav className="absolute right-1 top-1">
-        <ModeToggle/>
+        <ModeToggle />
       </nav>
       <aside className="h-screen hidden lg:block">
-        <Image src="/login.png" alt="login" width={554} height={832}/>
+        <Image src="/login.png" alt="login" width={554} height={832} />
       </aside>
       <div>
         <div className="size-[75px] bg-rose-600 rounded-full"></div>
         <h1 className="text-4xl font-extrabold uppercase">Sphere</h1>
-        <Tabs defaultValue="entrar" className="w-[400px]">      
-          <TabsList className="grid w-full grid-cols-2">        
-            <TabsTrigger value="entrar">Entrar</TabsTrigger>        
-            <TabsTrigger value="registrar">registrar</TabsTrigger>      
-          </TabsList>      
+        <Tabs defaultValue="entrar" className="w-[400px]">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="entrar">Entrar</TabsTrigger>
+            <TabsTrigger value="registrar">registrar</TabsTrigger>
+          </TabsList>
           <TabsContent value="entrar">
-            <h1>Form de login</h1>        
-          </TabsContent>      
-          <TabsContent value="registrar"> 
-            <h1>form de conta</h1>        
-          </TabsContent>    
+            <form action="" className="flex flex-col items-center gap-4">
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input type="email" id="email" placeholder="Email" />
+              </div>
+              <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" id="password" />
+              </div>
+              <Button>entrar</Button>
+            </form>
+          </TabsContent>
+          <TabsContent value="registrar">
+            <h1>form de conta</h1>
+          </TabsContent>
         </Tabs>
-        
+
       </div>
     </main>
   );
